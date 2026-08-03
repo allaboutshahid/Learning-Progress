3e58880a6ee77ec715acc769c64d48fad50797c8
allaboutshahid <abdulshahid84868@gmail.com>
2026-08-03 14:53:07 +0530

    Add array traversal program

---
+++ traversel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a[50],size,i;
+    printf("Enter the size of an array: ");
+    scanf("%d",&size);
+    printf("Enter the elements :\n");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("The elements are :\n");
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
