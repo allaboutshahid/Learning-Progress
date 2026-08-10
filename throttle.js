58dc6f886d9de3a88f818b106cf114a2c6024fd3
allaboutshahid <abdulshahid84868@gmail.com>
2026-08-10 22:42:56 +0530

    Add throttle js program

---
+++ throttle.js
@@ -0,0 +1,18 @@
+function throttle(fn, limit) {
+  let inThrottle = false;
+  return function (...args) {
+    if (!inThrottle) {
+      fn.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+// Example usage
+window.addEventListener(
+  "scroll",
+  throttle(() => console.log("Scroll event:", Date.now()), 300)
+);
+
+module.exports = throttle;
